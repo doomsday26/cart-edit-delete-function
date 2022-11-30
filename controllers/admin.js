4c993059165f215108838f0ad87753f1f595b876
doomsday26 <harshnamdeo261999@gmail.com>
2022-11-30 23:09:00 +0530

    sql insert delete products

--- controllers/admin.js
+++ controllers/admin.js
@@ -53,9 +53,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save()
+  .then(()=>  
+  { res.redirect('/')
+})
+.catch(err=>console.log(err));
+
 };
 
 exports.getProducts = (req, res, next) => {
@@ -70,7 +74,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct=(req,res,next)=>{
   const prodId= req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products')
+  Product.deleteById(prodId).then(()=>{ res.redirect('/admin/products')}).catch(err=>{console.log(err);});
+ 
 
 }
