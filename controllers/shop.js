96a45da46d7a2e120041ddced3e4cbe32d7026fd
doomsday26 <harshnamdeo261999@gmail.com>
2022-12-08 14:31:56 +0530

    delete in carts

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,18 +48,48 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
+
+ req.user.getCart().then(cart=>{ return cart.getProducts().then(products=>{
   res.render('shop/cart', {
     path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+    pageTitle: 'Your Cart',
+    products:products
+  })
+ }).catch(err=>{console.log(err);})}).catch(err=>console.log(err))
+
+
+// .catch(err=>{console.log(err);})
+
+ 
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity=1;
+ req.user.getCart().then(cart=>{
+  fetchedCart=cart;
+  return cart.getProducts({where:{id:prodId}})
+ }).then(products=>{
+let product;
+if(products.length>0){
+  product=products[0]
+}
+
+if(product){
+const oldquantity=product.cartItem.quantity;
+newQuantity=oldquantity+1;
+return product}
+return Product.findByPk(prodId)
+})
+.then(  product=>{
+    return fetchedCart.addProduct(product,{through:{quantity:newQuantity}
+    });
+})
+ .then(()=>{
+  res.redirect('/cart')
+ })
+ .catch(err=>{console.log(err);})
 };
 
 exports.getOrders = (req, res, next) => {
@@ -75,3 +105,23 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+
+
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.getCart().then(
+    cart=>{
+      return cart.getProducts({where:{id:prodId}})
+    } ).then(products=>{
+      const product= products[0];
+      product.cartItem.destroy();
+    }).then(
+      result=>{
+        res.redirect('/cart')
+      }
+    )
+    .catch(err=>{console.log(err);})
+};
+
+
